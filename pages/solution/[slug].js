817bff30f4cdc18d99ae7f510e995acf193a892e
nipaaa <akternipa941@gmail.com>
2023-04-12 14:34:29 +0600

    solution dynamic page add

---
+++ pages/solution/[slug].js
@@ -0,0 +1,41 @@
+import BestWork from '@/components/Home/BestWork';
+import Brands from '@/components/Home/Brands';
+import ContactForm from '@/components/Home/ContactForm';
+import FAQ from '@/components/Home/FAQ';
+import Technologies from '@/components/Home/Technologies';
+import Testimonial from '@/components/Home/Testimonial';
+import SolutionAnim from '@/components/Solution/SolutionAnim';
+import SolutionGames from '@/components/Solution/SolutionGames';
+import SolutionHero from '@/components/Solution/SolutionHero';
+import SolutionSteps from '@/components/Solution/SolutionSteps';
+import SolutionWorking from '@/components/Solution/SolutionWorking';
+import SolutionWhyUs from '@/components/Solution/WhyUs';
+import faqs from '@/utils/faqs-solution';
+import solutions from '@/utils/solutions';
+
+import { useRouter } from 'next/router';
+import React from 'react';
+
+const solutionItem = () => {
+    const router = useRouter();
+    const {slug}=router.query;
+    const content = solutions[slug];
+    return (
+        <>
+     <SolutionHero content={content} />
+      <SolutionAnim title={content?.title} />
+      <SolutionSteps title={content?.title} />
+      <SolutionGames />
+      <Technologies />
+      <SolutionWorking />
+      <SolutionWhyUs />
+      <Testimonial />
+      <BestWork />
+      <Brands />
+      <ContactForm />
+      <FAQ questions={faqs(content?.title)} />
+        </>
+    );
+};
+
+export default solutionItem;
