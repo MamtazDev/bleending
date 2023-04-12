6040f39d472b1adfcdfa98c5bf23a75e2f154d4c
nipaaa <akternipa941@gmail.com>
2023-04-12 10:59:20 +0600

    update

--- pages/services.js
+++ pages/services.js
@@ -1,6 +1,11 @@
+import Brands from '@/components/Home/Brands';
+import ContactForm from '@/components/Home/ContactForm';
+import FAQ from '@/components/Home/FAQ';
 import ServiceAward from '@/components/Services/ServiceAward';
 import ServiceHero from '@/components/Services/ServiceHero';
+import ServiceLatestGames from '@/components/Services/ServiceLatestGames';
 import ServiceTabs from '@/components/Services/ServiceTabs';
+import ServiceWhyUs from '@/components/Services/ServiceWhyUs';
 import React from 'react';
 
 const services = () => {
@@ -9,6 +14,11 @@ const services = () => {
            <ServiceHero/>
            <ServiceAward/>
            <ServiceTabs/> 
+           <ServiceWhyUs/>
+           <ServiceLatestGames/>
+           <Brands/>
+           <ContactForm/>
+           <FAQ/>
         </div>
     );
 };
